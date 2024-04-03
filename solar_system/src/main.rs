cf644f0a1d2cce31f71b48daa0a11c2353f03dbb
Alexandre <allespag@student.42.fr>
2024-04-04 01:53:48 +0200

    feat: add newton's law of gravitation

--- solar_system/src/main.rs
+++ solar_system/src/main.rs
@@ -12,44 +12,45 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let mut simulation = simulation::Simulation::new();
+    
     let sun = simulation::Body::new(
-        1988500e24f64,
+        1.9885e30_f64,
         10_000., // wrong value, in km
-        Vec3::ZERO,
-        Vec3::ZERO,
+        DVec3::ZERO,
+        DVec3::ZERO,
         YELLOW,
     );
+    simulation.add_body(sun);
 
     let mercury = simulation::Body::new(
-        0.330e24f64,
+        0.330e24_f64,
         4_879. / 2., // in km
-        Vec3::new(57.9e6f32, 0., 0.),
-        Vec3::ZERO,
+        DVec3::new(57.9e6_f64 * 1000., 0., 0.), // should be in meters?
+        DVec3::new(0., -47.4 * 1000., 0.),
         DARKGRAY,
     );
-
-    let venus = simulation::Body::new(
-        4.87e24f64,
-        12_104. / 2., // in km,
-        Vec3::new(108.2e6f32, 0., 0.),
-        Vec3::ZERO,
-        WHITE,
-    );
-
-
-    let mut simulation = simulation::Simulation::new();
-    simulation.add_body(sun);
     simulation.add_body(mercury);
-    simulation.add_body(venus);
+
+    // let venus = simulation::Body::new(
+    //     4.87e24f32,
+    //     12_104. / 2., // in km,
+    //     Vec3::new(108.2e6f32, 0., 0.),
+    //     Vec3::ZERO,
+    //     WHITE,
+    // );
+    // simulation.add_body(venus);
+
+    const DESIRED_FPS: f32 = 30.;
 
     loop {
-        clear_background(BLACK);
+        // clear_background(BLACK);
 
         simulation.update();
         simulation.draw();
 
         // From https://github.com/not-fl3/macroquad/issues/380
-        let minimum_frame_time = 1. / 60.; // 60 FPS
+        let minimum_frame_time = 1. / DESIRED_FPS;
         let frame_time = get_frame_time();
         if frame_time < minimum_frame_time {
             let time_to_sleep = (minimum_frame_time - frame_time) * 1000.;
--- solar_system/src/simulation.rs
+++ solar_system/src/simulation.rs
@@ -1,39 +1,29 @@
-// Newton's law of universal gravitation
-// F = G * ((m1 * m2) / r^2)
-
-// Newtonian constant of gravitation
-const G: f64 = 6.67430e-11f64;
-
-// Astronomical unit in km
-const AU: f32 = 149.6e6;
-
-// TODO: this is a random value, fix this
-const SCALE: f32 = 1. / 300000.;
-
 use macroquad::prelude::*;
 
+// Newtonian constant of gravitation
+const G: f64 = 6.67430e-11_f64;
+
+// Astronomical unit in meters
+pub const AU: f64 = 149.6e6_f64 * 1000.;
+
+// TODO: this is a random value, fix this
+const SCALE: f64 = 250. / AU;
+
+#[derive(Clone, Copy)]
 pub struct Body {
     mass: f64,
     radius: f64,
-    pos: Vec3,
-    initial_velocity: Vec3,
-    current_velocity: Vec3,
+    pos: DVec3,
+    current_velocity: DVec3,
     color: Color,
 }
 
 impl Body {
-    pub fn new(
-        mass: f64,
-        radius: f64,
-        pos: Vec3,
-        initial_velocity: Vec3,
-        color: Color,
-    ) -> Body {
+    pub fn new(mass: f64, radius: f64, pos: DVec3, initial_velocity: DVec3, color: Color) -> Body {
         Body {
             mass: mass,
             radius: radius,
             pos: pos,
-            initial_velocity: initial_velocity,
             current_velocity: initial_velocity,
             color: color,
         }
@@ -43,23 +33,43 @@ impl Body {
         unimplemented!();
     }
 
-    pub fn update(&self) {
-        unimplemented!();
+    pub fn attraction(&self, other: &Body) -> DVec3 {
+        let distance_x = other.pos.x - self.pos.x;
+        let distance_y = other.pos.y - self.pos.y;
+        let distance = f64::sqrt(distance_x * distance_x + distance_y * distance_y);
+
+        let force = G * (self.mass * other.mass) / (distance * distance);
+        let theta = f64::atan2(distance_y, distance_x);
+        let force_x = f64::cos(theta) * force;
+        let force_y = f64::sin(theta) * force;
+
+        return DVec3::new(force_x, force_y, 0.);
+    }
+
+    pub fn update(&mut self, sun: &Body, timestep: f64) {
+        // DO NOT WORK
+        if std::ptr::eq(self, sun) {
+            return;
+        }
+
+        let total_force = self.attraction(sun);
+        self.current_velocity += total_force / self.mass * timestep;
+        self.pos += self.current_velocity * timestep;
     }
 
     pub fn draw(&self) {
-        let x = self.pos.x * SCALE + screen_width() / 2.0;
-        let y = self.pos.y * SCALE + screen_height() / 2.0;
+        let offset = DVec3::new(
+            (screen_width() / 2.) as f64,
+            (screen_height() / 2.) as f64,
+            0.,
+        );
+        let x: f32 = (self.pos.x * SCALE + offset.x) as f32;
+        let y: f32 = (self.pos.y * SCALE + offset.y) as f32;
 
         // TODO: this is a random value, fix this
         let r = self.radius as f32 / 100.;
 
-        draw_circle(
-            x,
-            y,
-            r,
-            self.color,
-        );
+        draw_circle(x, y, r, self.color);
     }
 }
 
@@ -70,22 +80,22 @@ pub struct Simulation {
 }
 
 // TODO: learn more about "Default"
-impl Default for Simulation {
-    fn default() -> Self {
-        Self {
-            bodies: Vec::new(),
-            time: 0,
-            timestep: 1,
-        }
-    }
-}
+// impl Default for Simulation {
+//     fn default() -> Self {
+//         Self {
+//             bodies: Vec::new(),
+//             time: 0,
+//             timestep: 1,
+//         }
+//     }
+// }
 
 impl Simulation {
     pub fn new() -> Simulation {
         Simulation {
             bodies: Vec::new(),
             time: 0,
-            timestep: 60 * 60 * 24,
+            timestep: 24 * 60 * 60,
         }
     }
 
@@ -98,11 +108,24 @@ impl Simulation {
     }
 
     pub fn update(&mut self) {
+        let sun = self.bodies[0];
+        for body in self.bodies.iter_mut() {
+            // TODO: temporary, i.e, we're dealing with the sun
+            if body.color == YELLOW {
+                continue;
+            }
 
+            body.update(&sun, self.timestep as f64);
+        }
+        self.time += self.timestep;
     }
 
     pub fn draw(&self) {
         for body in self.bodies.iter() {
+            // TODO: temporary, i.e, we're dealing with the sun
+            // if body.color == YELLOW {
+            //     continue;
+            // }
             body.draw();
         }
     }
