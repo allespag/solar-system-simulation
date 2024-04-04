beb60e5004bb948a6868ccf14af896cb95daa697
Alexandre <allespag@student.42.fr>
2024-04-05 00:14:14 +0200

    feat(main): add jupiter ; NOTE: jupiter is too big for this simulation TODO: represent body with arbitrary radius

--- solar_system/src/main.rs
+++ solar_system/src/main.rs
@@ -29,7 +29,7 @@ async fn main() {
         0.330e24_f64,
         4_879. / 2., // in km
         DVec3::new(57.9e6_f64 * 1000., 0., 0.),
-        DVec3::new(0., -47.4 * 1000., 0.),
+        DVec3::new(0., 47.4 * 1000., 0.),
         DARKGRAY,
     );
     simulation.add_body(mercury);
@@ -39,7 +39,7 @@ async fn main() {
         4.87e24_f64,
         12_104. / 2., // in km
         DVec3::new(108.2e6_f64 * 1000., 0., 0.),
-        DVec3::new(0., -35.02 * 1000., 0.),
+        DVec3::new(0., 35.02 * 1000., 0.),
         WHITE,
     );
     simulation.add_body(venus);
@@ -53,6 +53,27 @@ async fn main() {
         BLUE,
     );
     simulation.add_body(earth);
+
+    let mars = simulation::Body::new(
+        simulation::BodyType::PLANET,
+        0.642e24_f64,
+        6_792. / 2., // in km
+        DVec3::new(228.0e6_f64 * 1000., 0., 0.),
+        DVec3::new(0., 24.1 * 1000., 0.),
+        RED,
+    );
+    simulation.add_body(mars);
+
+    let jupiter = simulation::Body::new(
+        simulation::BodyType::PLANET,
+        1898e24_f64,
+        142_984. / 2., // in km
+        DVec3::new(778.5e6_f64 * 1000., 0., 0.),
+        DVec3::new(0., 13.1 * 1000., 0.),
+        BROWN,
+    );
+    simulation.add_body(jupiter);
+
 
     const DESIRED_FPS: f32 = 60.;
 
